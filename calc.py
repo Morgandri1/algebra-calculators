ed830f545180859225a9c1ffb19086cff9393992
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2022-02-07 23:52:39 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,27 +1,15 @@
+import math
 from sys import argv
-constant = argv[2]
-x = argv[3]
-y = argv[4]
 
-if argv[1] == "inv":
-    if y == "y":
-        eq = float(constant) / float(x)
-        print(eq)
-    if constant == "k":
-        eq = float(y) * float(x)
-        print(eq)
-    if x == "x":
-        eq = float(y) * float(constant)
-        print(eq)
-if argv[1] == "dir": 
-    if y == "y":
-        eq = float(constant) * float(x)
-        print(eq)
-    if constant == "k":
-        eq = float(y) / float(x)
-        print(eq)
-    if x == "x":
-        eq = float(y) / float(constant)
-        print(eq)
-else:
-    print("invalid operation, use either inv or dir, use -h for help")
+r = float(argv[2])
+d = r*2
+
+pi = math.pi
+
+if argv[1] == "a":
+    eq = pi*r**2
+    print(eq)
+if argv[1] == "c":
+    eq = 2*pi*r
+    print(eq)
+
